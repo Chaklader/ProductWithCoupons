ce26f48109bba50039a57a26421a9e3f1cb5f053
Chaklader A. Arefe <omi.chaklader@gmail.com>
2021-11-14 17:31:43 +0600

    created the authorization server

--- CouponService/src/main/java/com/example/coupon/security/AuthorizationServerConfig.java
+++ CouponService/src/main/java/com/example/coupon/security/AuthorizationServerConfig.java
@@ -3,7 +3,10 @@ package com.example.coupon.security;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,10 +15,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.*;
 
 import javax.sql.DataSource;
+import java.security.KeyPair;
 
 
 @Configuration
@@ -39,21 +43,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	private DataSource dataSource;
 
 
+	@Value("${keyFile}")
+	private String keyFile;
 
-//	@Value("${keyFile}")
-//	private String keyFile;
-//	@Value("${password}")
-//	private String password;
-//	@Value("${alias}")
-//	private String alias;
+	@Value("${password}")
+	private String password;
 
+	@Value("${alias}")
+	private String alias;
 
-
-//	@Override
-//	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-//		endpoints.tokenStore(tokenStore()).accessTokenConverter(jwtAccessTokenConverter())
-//				.authenticationManager(authenticationManager).userDetailsService(userDetailsService);
-//	}
 
 //	@Override
 //	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
@@ -63,39 +61,49 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 //	}
 
 
+//	@Override
+//	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//
+//		endpoints.tokenStore(new JdbcTokenStore(dataSource))
+//				.authenticationManager(authenticationManager).userDetailsService(userDetailsService);
+//	}
+
+
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-
-		endpoints.tokenStore(new JdbcTokenStore(dataSource))
+		endpoints.tokenStore(tokenStore()).accessTokenConverter(jwtAccessTokenConverter())
 				.authenticationManager(authenticationManager).userDetailsService(userDetailsService);
 	}
+
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory().withClient("couponclientapp").secret(passwordEncoder.encode("9999"))
 				.authorizedGrantTypes("password", "refresh_token").scopes("read", "write").resourceIds(RESOURCE_ID);
 	}
-	
+
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		security.tokenKeyAccess("permitAll()");
 	}
-	
 
-//	@Bean
-//	public TokenStore tokenStore() {
-//		return new JwtTokenStore(jwtAccessTokenConverter());
-//	}
-//
-//	@Bean
-//	public JwtAccessTokenConverter jwtAccessTokenConverter() {
-//		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-//		KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource(keyFile),
-//				password.toCharArray());
-//		KeyPair keyPair = keyStoreKeyFactory.getKeyPair(alias);
-//		jwtAccessTokenConverter.setKeyPair(keyPair);
-//		return jwtAccessTokenConverter;
-//
-//	}
+
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(jwtAccessTokenConverter());
+	}
+
+	@Bean
+	public JwtAccessTokenConverter jwtAccessTokenConverter() {
+
+		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource(keyFile),
+				password.toCharArray());
+
+		KeyPair keyPair = keyStoreKeyFactory.getKeyPair(alias);
+		jwtAccessTokenConverter.setKeyPair(keyPair);
+
+		return jwtAccessTokenConverter;
+	}
 
 }

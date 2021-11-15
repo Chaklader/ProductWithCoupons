95edfe5c5ea33f975693d7c9018e314d879d7679
Chaklader A. Arefe <omi.chaklader@gmail.com>
2021-11-16 01:57:39 +0600

    updated for the final commit

---
+++ AuthorizationServer/src/main/java/com/example/authorization/AuthorizationServer.java
@@ -0,0 +1,14 @@
+package com.example.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class AuthorizationServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServer.class, args);
+    }
+}

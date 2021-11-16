0a276245891d4308dc06c4f5668b75cac4c0b393
Chaklader A. Arefe <omi.chaklader@gmail.com>
2021-11-16 09:04:20 +0600

    added the code

--- CouponService/src/main/java/com/example/coupon/security/ResourceServerConfig.java
+++ CouponService/src/main/java/com/example/coupon/security/ResourceServerConfig.java
@@ -26,6 +26,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Value("${publicKey}")
 	public String publicKey;
 
+	@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+	String issuerUri;
+
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -54,5 +57,24 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 		return jwtAccessTokenConverter;
 	}
+
+
+
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//
+//		http
+//				.authorizeRequests(authorizeRequests ->
+//						authorizeRequests
+//								.antMatchers("/services").hasAuthority("SCOPE_services:read")
+//								.anyRequest().authenticated()
+//				)
+//				.oauth2ResourceServer(oauth2ResourceServer ->
+//						oauth2ResourceServer
+//								.jwt(jwt ->
+//										jwt.decoder(JwtDecoders.fromIssuerLocation(issuerUri))
+//								)
+//				);
+//	}
 
 }
